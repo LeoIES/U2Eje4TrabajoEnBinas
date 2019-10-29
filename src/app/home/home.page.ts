7548dc92de3799e128f7e21099d33bdfed281fb1
Leo Estrada <ivleestradasa@ittepic.edu.mx>
2019-10-29 11:36:59 -0700

    Errores de validacion corregidos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, Validator, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  myForm: FormGroup;
 
+  constructor( private fb:FormBuilder) {}
+
+  ngOnInit()
+  {
+    this.myForm=this.fb.group({
+        //required que es obligatoria
+      rfc: ["", Validators.compose([Validators.required,Validators.maxLength(18),Validators.minLength(13), Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])],
+      name: ["", Validators.compose([Validators.required])],
+      email: ["", Validators.compose([Validators.required, Validators.maxLength(320), Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+      genero:["", Validators.required],
+      curp:["", Validators.compose([Validators.required, Validators.minLength(18), Validators.maxLength(18), Validators.pattern('[A-Z][A,E,I,O,U,X][A-Z]{2}[0-9]{2}[0-1][0-9][0-3][0-9][M,H][A-Z]{2}[B,C,D,F,G,H,J,K,L,M,N,Ñ,P,Q,R,S,T,V,W,X,Y,Z]{3}[0-9,A-Z][0-9]')])]
+    });
+  }
 }
